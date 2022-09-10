704cd855d288676cc76af9e889b536f461fcddb6
olgasavitska <savitskaoo9@ukr.net>
2022-09-10 15:20:57 +0300

    optimization banner

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           text={
             "What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving."
           }
-          bgImg={"../../img/banner.jpg"}
+          bgImg={"../../img/banner.jpeg"}
         ></Banner>
       </ContainerComponent>
       <ContainerComponent>
